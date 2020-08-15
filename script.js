77d4044d6fe384107c817ab4ce8a3deacb31bf64
mahirr476 <rmahirr476@gmail.com>
2020-08-15 18:46:22 -0400

    format edits

---
+++ script.js
@@ -0,0 +1,13 @@
+// Nav bar scroll animation
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navBar").style.top = "0";
+  } else {
+    document.getElementById("navBar").style.top = "-50px";
+  }
+}
+
+scrollFunction();
